bd9c2aab92a8158196224f1f2c1bee165a66af0e
William Castro <william.castro2000@outlook.com>
2024-02-08 00:34:07 -0500

    made some minor modifications within my learning

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+    <div>
+        <div className="wrapper-header">
+            <h1 className="header">Hello, This is my Header module being displayed!</h1>
+        </div>
+    </div>
+    );
+}
+export default Header;
